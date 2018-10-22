cc5440bfc4e6e5fab9d33272ca82667e3f2aaa6e
Bartosz Ciulkin <Bartosz.ciulkin5@orange.com>
2018-10-22 17:02:33 +0200

    Refreshes recyclerview

--- app/src/main/java/bartek/criminalintent/CrimeListFragment.java
+++ app/src/main/java/bartek/criminalintent/CrimeListFragment.java
@@ -36,6 +36,12 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private TextView mTitleTv;
@@ -104,7 +110,11 @@ public class CrimeListFragment extends Fragment {
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if (mAdapter == null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
     }
 }
